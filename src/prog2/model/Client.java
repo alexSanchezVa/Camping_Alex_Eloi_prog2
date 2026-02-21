47028121911f67437c72de67c51ae9290de4e7d1
alexSanchezVa <asanchva257@alumnes.ub.edu>
2026-02-21 17:43:05 +0100

    Finalizado sin revisar

--- src/prog2/model/Client.java
+++ src/prog2/model/Client.java
@@ -1,5 +1,27 @@
 package prog2.model;
 
 public class Client implements InClient{
+    private String nom;
+    private String dni;
 
+    public Client(String nom, String dni){
+        this.nom = nom;
+        this.dni = dni;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
 }
